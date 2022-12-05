e281e0342ee76ff693a8f80e6eedc7463b76970d
Amir Fahimnia <info@faktor22.nl>
2022-12-05 22:15:22 +0100

    Used Getter and Setter Add Client ID, FirstName and LastName Changed Java version to 11 Add dependency (h2database)

--- src/main/java/com/example/javademo/domain/Client.java
+++ src/main/java/com/example/javademo/domain/Client.java
@@ -1,4 +1,26 @@
 package com.example.javademo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
 }

256c162d72229c6a7b61e6d6a6300800ede6a3bb
Amir Fahimnia <info@faktor22.nl>
2022-12-06 15:41:40 +0100

    Add ClientRepository Connect ClientRepository to ClientService using @Service

--- src/main/java/com/example/javademo/repository/ClientRepository.java
+++ src/main/java/com/example/javademo/repository/ClientRepository.java
@@ -1,4 +1,14 @@
 package com.example.javademo.repository;
 
-public class ClientRepository {
+import com.example.javademo.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    @Override
+    Optional<Client> findById(Long id);
 }
--- src/main/java/com/example/javademo/services/ClientService.java
+++ src/main/java/com/example/javademo/services/ClientService.java
@@ -1,4 +1,30 @@
 package com.example.javademo.services;
 
+import com.example.javademo.domain.Client;
+import com.example.javademo.repository.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
 public class ClientService {
+
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public List<Client> findAllClient() {
+        return clientRepository.findAll();
+    }
+
+    public void addClient(Client client) {
+        clientRepository.save(client);
+    }
+
+    public Client findClientById(Long id) {
+        return null;
+    }
 }
